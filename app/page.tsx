ae6ef335497bf9106cae37771821e52bcb21b97e
MmYYAIS <39291075+MmYYAIS@users.noreply.github.com>
2023-09-01 15:28:56 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
 import type { FC } from 'react'
 import React from 'react'
+import { GetServerSideProps } from 'next'
 
-import type { IMainProps } from '@/app/components'
-import Main from '@/app/components'
+const SECRET_TOKEN = `${process.env.token}`; // 你可以把这个值放在环境变量中
 
-const App: FC<IMainProps> = ({
-  params,
-}: any) => {
-  return (
-    <Main params={params} />
-  )
+const Home: FC = () => {
+  return <div>Welcome to the protected homepage!</div>
 }
 
-export default React.memo(App)
+export default React.memo(Home)
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const token = context.query.token;
+
+  if (token !== SECRET_TOKEN) {
+    return {
+      redirect: {
+        destination: '/error', // 或其他错误页面
+        permanent: false,
+      },
+    }
+  }
+
+  // 如果验证成功，返回你的页面 props
+  return {
+    props: {}, 
+  }
+}
